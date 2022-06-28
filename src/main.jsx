21d8e8fc4b1e66e8d42caab1830911176d555cd9
Ivan <ivanfrancoarg@gmail.com>
2022-06-28 11:27:33 -0300

    subiendo lo que hice para casa

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App'
+import { HeroesApp } from './HeroesApp'
+import './style.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HeroesApp />
+  </React.StrictMode>
+)
