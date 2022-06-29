e393a5e757db6f9ff6e02b5a2dbedfc4a45b677d
IvanFranco1702 <ivanfrancoarg@gmail.com>
2022-06-28 23:14:31 -0300

    clase 190 creo incompleta

--- src/interfasGrafica/components/NavBar.jsx
+++ src/interfasGrafica/components/NavBar.jsx
@@ -1,7 +1,13 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
+
+    const navigate = useNavigate
+    
+    const onlogout =()=>{
+        console.log('deslogueando')
+    }
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
@@ -45,7 +51,8 @@ export const Navbar = () => {
                     <span className="nav-item nav-link text-primary">
                         ivan 
                     </span>
-                    <button className="nav-item nav-link btn">
+                    <button className="nav-item nav-link btn"
+                    onClick={onlogout}>
                      Logout
                     </button>
                    
