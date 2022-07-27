270e543f7835cfe4db3feec3af62509f742a647d
Ivan <ivanfrancoarg@gmail.com>
2022-07-27 15:47:46 -0300

    todo

--- Curso/react-journal-material-fin-seccion-19/src/auth/layout/AuthLayout.jsx
+++ Curso/react-journal-material-fin-seccion-19/src/auth/layout/AuthLayout.jsx
@@ -13,22 +13,22 @@ export const AuthLayout = ({ children, title = '' }) => {
       sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
     >
 
-      <Grid item
-       className='box-shadow'
-       xs={ 3 }
-       sx={{ 
-            width: { sm: 450 },
-            backgroundColor: 'white', 
-            padding: 3, 
-            borderRadius: 2 
-        }}>
-          
-          <Typography variant='h5' sx={{ mb: 1 }}>{ title }</Typography>
-
+        <Grid item
+        className='box-shadow'
+        xs={ 3 }
+        sx={{ 
+              width: { sm: 450 },
+              backgroundColor: 'white', 
+              padding: 3, 
+              borderRadius: 2 
+          }}>
             
-            { children }
+            <Typography variant='h5' sx={{ mb: 1 }}>{ title }</Typography>
 
-        </Grid>
+              
+              { children }
+
+          </Grid>
 
     </Grid>
 
